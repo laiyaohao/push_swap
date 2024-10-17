2cb5a132c4138be2f6d6b0edbd524e24f65aa63f
laiyaohao <laiyaohao96@gmail.com>
2024-10-17 09:32:24 +0800

    chore: added logging to see error

--- radix_sort.c
+++ radix_sort.c
@@ -21,7 +21,7 @@ int	find_max_bit(long max_num)
 	int	max_bits;
 
 	max_bits = 0;
-	while ((max_num >> max_bits))
+	while ((max_num >> max_bits) != 0)
 		max_bits++;
 	return (max_bits);
 }
@@ -35,11 +35,11 @@ void	radix_inner_sort(int max_bits, t_stack *stack_a, t_stack *stack_b)
 	node = stack_a->top;
 	while (i < max_bits)
 	{
+		
 		while (node != NULL)
 		{
 			long num = *(long *)node->content;
 			node = node->next;
-
 			// Check the i-th bit of the number
 			if (((num >> i) & 1) == 0) {
 				pb(stack_b, stack_a); // Push to stackB if the i-th bit is 0
@@ -47,20 +47,33 @@ void	radix_inner_sort(int max_bits, t_stack *stack_a, t_stack *stack_b)
 				ra(stack_a, 1);  // Rotate if the i-th bit is 1
 			}
 		}
+		ft_printf("stack a: ");
+		print_stack(stack_a);
+		ft_printf("stack b: ");
+		print_stack(stack_b);
 
 		// Push all elements back from stackB to stackA
 		while (!is_empty(stack_b)) {
 			pa(stack_a, stack_b);
 		}
 		i++;
+		ft_printf("\n\n");
+		ft_printf("stack a: ");
+		print_stack(stack_a);
+		ft_printf("stack b: ");
+		print_stack(stack_b);
+		if (node == NULL)
+			ft_printf("stack_a->top: %d\n",*(long *)stack_a->top->content);
+		// node = stack_a->top;
 	}
 }
 
 void radix_sort(t_stack *stack_a, t_stack *stack_b)
 {
 	long max_num = find_largest(stack_a);
+	ft_printf("max_num: %d\n", max_num);
 	int max_bits = find_max_bit(max_num);
-
+	ft_printf("max_bits: %d\n", max_bits);
 	// for (int i = 0; i < max_bits; i++) {
 	// 	for (int j = 0; j < size; j++) {
 	// 		int num = stack_a->items[stack_a->top];
